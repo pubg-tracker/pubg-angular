cfb3c0aee7927e3b3ffcc6ae4efda99f5fdcca58
Ananth <anantharuna110@gmail.com>
2022-04-14 09:18:04 +0530

    hiding nav buttons when logged in

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { AuthenticationService } from '../services/authentication.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  loggedIn: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private authService: AuthenticationService,private router: Router) {}
 
   ngOnInit(): void {
+    const token = this.authService.getBearerToken();
+    this.authService.isUserAuthenticated(token).then(
+      (authenticated: boolean) => {
+        if (authenticated) {
+          this.loggedIn = true;
+        } else {
+          this.loggedIn = false;
+        }
+      }
+    );
   }
 
   navigateTo(route: string) {
@@ -19,5 +30,4 @@ export class HeaderComponent implements OnInit {
     }
     this.router.navigate([route]);
   }
-
 }
